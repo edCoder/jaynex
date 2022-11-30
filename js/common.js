6ea2efcfb0ba0f5bd0b1ee0f3b5f2f9e983b5da2
Reni <renibabu3@gmail.com>
2022-12-01 03:28:45 +0530

    updations

--- js/common.js
+++ js/common.js
@@ -167,3 +167,14 @@ $(document).ready(function () {
         }
     });
 });
+
+
+
+jQuery(document).ready(function ($) {
+
+    $('.thumb-img a').on("click", function(e){
+        e.preventDefault();
+        $('.thumb-previewer img').attr("src", $(this).attr("href"));
+    });
+
+});
